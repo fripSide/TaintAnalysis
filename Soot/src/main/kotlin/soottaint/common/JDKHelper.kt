3caf0e00f6496befda64de97489cb1a5bebda14e
writecode <clangllvm@163.com>
2022-01-14 23:28:54 +0800

    pass 0

---
+++ Soot/src/main/kotlin/soottaint/common/JDKHelper.kt
@@ -0,0 +1,11 @@
+package soottaint.common
+
+object JavaSDKHelper {
+
+	val methods = listOf("")
+
+	fun isMethodTaint(): Boolean {
+
+		return false
+	}
+}
---
+++ Soot/src/main/kotlin/soottaint/common/VirtualCallResolve.kt
@@ -0,0 +1,4 @@
+package soottaint.common
+
+class VirutalCallResolve {
+}
